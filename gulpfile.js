c45cd58224637ac3414b7fe82e04aec05b4bed1c
Steve Higgs <shiggs@opentable.com>
2015-08-27 10:42:36 +0100

    Compile coffee to raw js at publish time to stop the coffee script parser being pulled into all consumers

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,9 @@
 // Include gulp
 var gulp = require('gulp');
 var gulpSequence = require('gulp-sequence');
+var sourcemaps = require('gulp-sourcemaps');
+var coffee = require('gulp-coffee');
+var gutil = require('gulp-util');
 
 var scripts = ['src/**/*.coffee', 'src/**/*.js'];
 var tests = ['test/**/*.coffee', 'test/**/*.js'];
@@ -11,11 +14,11 @@ var mochaOpts = {
   require: ['./test/globals.js']
 };
 var coverageThresholds = {
-    statements: 99.9,
-    branches: 98.5,
-    functions: 99.9,
-    lines: 99.9
-  };
+  statements: 99.9,
+  branches: 98.5,
+  functions: 99.9,
+  lines: 99.9
+};
 
 require('ot-gulp-release-tasks')(gulp, scripts, tests, mochaOpts, coverageThresholds);
 
@@ -26,3 +29,11 @@ gulp.task('test', ['ot-release-coverage-test']);
 gulp.task('test-all', gulpSequence('ot-release-no-coverage-test', 'ot-release-coverage-test'));
 
 gulp.task('lint', ['ot-release-lint-coffee']);
+
+gulp.task('build-coffee', function () {
+  gulp.src('./src/*.coffee')
+    .pipe(sourcemaps.init())
+    .pipe(coffee({bare: true}).on('error', gutil.log))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./lib/'));
+});
